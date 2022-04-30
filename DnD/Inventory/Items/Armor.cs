25d46a1a187d461e02fb423a9e47f9d172a2239d
Michael <42883348+Froderick-Fronkensteen@users.noreply.github.com>
2022-04-30 19:19:21 -0400

    Created Armor Class

---
+++ DnD/Inventory/Items/Armor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DnD.Inventory.Items
+{
+    public class Armor : Item
+    {
+        // Time it takes to put armor on
+        int don = 0;
+        
+        // Time it takes to take armor off
+        int doff = 0;
+        
+    }
+}
