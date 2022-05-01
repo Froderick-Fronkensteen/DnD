28a274d24424708d37b6e69b59dbc520741e9cc3
Froderick-Fronkensteen <42883348+Froderick-Fronkensteen@users.noreply.github.com>
2022-05-01 00:50:20 -0400

    Added comments to the item.cs class

--- DnD/Inventory/Items/Item.cs
+++ DnD/Inventory/Items/Item.cs
@@ -16,29 +16,46 @@ namespace DnD
         // Weight of the item (in ounces)
         protected double dWeight;
         
+        // Get the name of the item
         public string getName()
         {
             return sName;
         }
         
+        // Set the name of the item
         public void setName(string newName)
         {
             sName = newName;
         }
         
+        // Get the weight of the item
         public double getWeight()
         {
             return dWeight;
         }
         
+        // Get the weight but get it in string format
         public string getWeightString()
         {
             return dWeight.ToString();
         }
         
+        // Set the weight of the item
         public void setWeight(double newWeight)
         {
             dWeight = newWeight;
+        }
+
+        // Get the cost of the item
+        public int getCost()
+        {
+            return iCost;
+        }
+
+        // Set the cost of the item
+        public void setCost(int newCost)
+        {
+            iCost = newCost;
         }
     }
 }
