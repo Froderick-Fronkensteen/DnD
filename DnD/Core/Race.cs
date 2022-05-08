b2544be724ba349017397b43f10af63dcf687fb4
Michael <42883348+Froderick-Fronkensteen@users.noreply.github.com>
2022-05-08 01:27:23 -0400

    Added Races Class

---
+++ DnD/Core/Race.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DnD.Core
+{
+    public class Race
+    {
+        // The age of the character
+        int age = 0;
+        
+        // Different types of alignment
+        private enum AlignmentType
+        {
+            Lawful_Good,
+            Neutral_Good,
+            Chaotic_Good,
+            Lawful_Neutral,
+            Neutral,
+            Chaotic_Neutral,
+            Lawful_Evil,
+            Neutral_Evil,
+            Chaotic_Evil
+        }
+        
+        // The height of the character
+        string height = null;
+        
+        // The speed of the character
+        int speed = 0;
+        
+        private enum Languages
+        {
+            Common,
+            Dwarvish,
+            Elvish,
+            Giant,
+            Gnomish,
+            Goblin,
+            Halfling,
+            Orc,
+            Abyssal,
+            Celestial,
+            Draconic,
+            Deep_Speech,
+            Infernal,
+            Primodial,
+            Sylvan,
+            Undercommon
+        }
+    }
+}
