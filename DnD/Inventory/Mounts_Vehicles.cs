def169ad340f16eb726d78690754f9bb359bed41
Michael <42883348+Froderick-Fronkensteen@users.noreply.github.com>
2022-05-08 00:10:56 -0400

    Created Mount_Vehicles and Character Class
    Created both of these class and mounts_vehicles is initially done.  The
    character class is just a bare class.

---
+++ DnD/Inventory/Mounts_Vehicles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DnD.Inventory
+{
+    public class Mounts_Vehicles : Item
+    {
+        // How fast can the mount or vehicle move
+        int speed = 0;
+        
+        // How much weight the mount or vehicle can carry
+        int carryingCapacity = 0;
+        
+        public Mounts_Vehicles(string name, Currency cost, int newSpeed, int newCarryingCapacity)
+        {
+            setName(name);
+            setCost(cost);
+            speed = newSpeed;
+            carryingCapacity = newCarryingCapacity;
+        }
+        
+        // Get the speed of the mount or vehicle
+        public int getSpeed()
+        {
+            return speed;
+        }
+        
+        // Get the carrying capacity of the mount or vehicle
+        public int getCarryingCapacity()
+        {
+            return carryingCapacity;
+        }
+    }
+}
